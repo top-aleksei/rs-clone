f882264fd6d3cf636f349bd03734bf0764d39ad4
poznerrr <39535687+poznerrr@users.noreply.github.com>
2023-02-28 11:13:10 +0300

    fix: fix socket connection

--- src/app/controller/socket.ts
+++ src/app/controller/socket.ts
@@ -2,6 +2,6 @@
 export let ws: WebSocket;
 
 export function createConnection() {
-  ws = new WebSocket('wss://mymonopoly.sytes.net');
+  ws = new WebSocket('ws://mymonopoly.sytes.net');
   // ws = new WebSocket('ws://localhost:14000');
 }
